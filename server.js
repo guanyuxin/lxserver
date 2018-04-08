e73ec9c7c98aa22b400f6a0917f102711269bde2
guanyuxin <guanyuxin@meituan.com>
2018-04-08 19:56:21 +0800

    e

--- server.js
+++ server.js
@@ -20,9 +20,17 @@ var server = app.listen(3000, function () {
 
 
 function saveRules(packageObj) {
-  fs.rename(__dirname+'/var/config.json', __dirname+'/var/config.json'+new Date().getDay(), ()=>{
-    fse.outputFile(__dirname+'/var/config.json', packageObj, (err, data) => {
-      console.log("updateed")
+  return new Promise((resolve, reject) => {
+    fs.rename(__dirname+'/var/config.json', __dirname+'/var/config.json'+new Date().getDay(), async ()=>{
+      await fse.outputFile(__dirname+'/var/config.json', packageObj);
+      try {
+        var v = parseInt(await fse.readFile(__dirname+'/var/version'));
+      } catch (e) {
+        var v = 1;
+      }
+      fse.outputFile(__dirname+'/var/version', "" + (v+1))
+      console.log("updateed v" + v);
+      resolve();
     });
   });
 }
@@ -33,8 +41,19 @@ app.get('/config', function(req, res) {
   res.sendFile(__dirname+'/var/config.json');
 })
 
-app.post('/config', function(req, res) {
+app.get('/configVersion', async function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
-  saveRules(req.body);
-  res.send("1");
+  try {
+    var v = parseInt(await fse.readFile(__dirname+'/var/version'));
+  } catch (e) {
+    var v = 1;
+  }
+  res.send("" + v);
+})
+
+
+app.post('/config', async function(req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  await saveRules(req.body);
+  res.send('{"status":0}');
 })
