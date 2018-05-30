b55d6bd8fc2c64830fe80d422c062b321fb56bf8
guanyuxin <guanyuxin@meituan.com>
2018-05-30 22:54:59 +0800

    inv

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.static('dist'));
 
 app.use('/client', express.static('client'));
+app.use('/invite', express.static('invite'));
 app.use(bodyParser.text())
 app.use(bodyParser.urlencoded({ extended: false, limit:'50mb' }))
 app.use(cookieParser());
